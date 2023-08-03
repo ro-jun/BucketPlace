d1632146d6f27794860e23170cf69b2b6d49088e
eunseopi <asxzqw12@naver.com>
2023-08-04 01:10:04 +0900

    ReBucket

--- app/src/views/screens/Write.js
+++ app/src/views/screens/Write.js
@@ -1,10 +1,42 @@
-import React from 'react';
-import {View, Text} from "react-native";
+import React, { useState } from 'react';
+import {View, Text, StyleSheet } from "react-native";
+import Body from '../../post/Body';
 
-const Write = () => (
-    <View style={{flex:1, justifyContent:"center", alignItems:"center"}}>
-        <Text>Write</Text>
-    </View>
-);
+const Write = () => {
+    const [posts, setPosts] = useState([]);
+  
+    const addPosts = (post) => {
+      const newPosts = {
+        id: Date.now(),
+        text: post,
+        completed: false,
+      };
+      setPosts((prevState) => [newPosts, ...prevState]);
+    };
+  
+    console.log(posts);
+  
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>Post</Text>
+        <Body />
+      </View>
+    );
+  };
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        paddingTop: 30,
+        backgroundColor: "#EEE",
+      },
+    title: {
+        fontWeight: "800",
+        fontSize: 30,
+        marginLeft: 20,
+        marginBottom: 20,
+    }
+})
 
 export default Write;
