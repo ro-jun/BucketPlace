69f572151124866e61254f7d74d8d1149a8ab31f
hyeonmin <hyunmin504@naver.com>
2023-08-07 00:40:13 +0900

    GPS-on

--- app/src/views/screens/PrintStart.js
+++ app/src/views/screens/PrintStart.js
@@ -1,13 +1,75 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
+import MapView, { Marker, Polyline } from 'react-native-maps';
 import * as Location from 'expo-location';
 
 const PrintStart = () => {
+  const [location, setLocation] = useState(null);
+  const [routeCoordinates, setRouteCoordinates] = useState([]);
+
+  useEffect(() => {
+    getLocationAsync();
+  }, []);
+
+  const getLocationAsync = async () => {
+    try {
+      // 위치 권한 요청
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.log('위치 권한이 거부되었습니다.');
+        return;
+      }
+
+      // 위치 변경 이벤트 리스너 등록
+      Location.watchPositionAsync(
+        {
+          distanceInterval: 10, // 10 미터마다 위치 변경 이벤트 발생
+        },
+        (location) => {
+          // 현재 위치 정보 업데이트
+          setLocation(location.coords);
+
+          // 경로 업데이트
+          setRouteCoordinates((prevCoordinates) => [
+            ...prevCoordinates,
+            {
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            },
+          ]);
+        }
+      );
+    } catch (error) {
+      console.error('현재 위치를 가져오는데 실패했습니다:', error);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>PrintStart 컴포넌트</Text>
-      {/* 원하는 내용을 추가하여 기록 시작 페이지를 꾸밀 수 있습니다. */}
+      {location && (
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: location.latitude,
+            longitude: location.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        >
+          <Marker
+            coordinate={{
+              latitude: location.latitude,
+              longitude: location.longitude,
+            }}
+            title="내 위치"
+          />
+          <Polyline
+            coordinates={routeCoordinates}
+            strokeWidth={2}
+            strokeColor="blue"
+          />
+        </MapView>
+      )}
     </View>
   );
 };
@@ -17,6 +79,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  map: {
+    flex: 1,
+    width: '100%',
   },
 });
 
